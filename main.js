ddfe02e7d2acb0b9631be117d8b90080c542facd
Dimonkalon <120312746+Dimonkalon@users.noreply.github.com>
2023-01-09 19:58:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      // добавим к кнопке класс btn-up_hide
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      // при прокрутке содержимого страницы
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      // при нажатии на кнопку .btn-up
+      document.querySelector('.btn-up').onclick = () => {
+        // переместим в начало страницы
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
+
+  let openButton = document.querySelector(".burger")
+let modalContainer = document.querySelector('.links')
+
+openButton.onclick = function() {
+    console.log("open")
+    modalContainer.style.display = 'flex'
+}
+
+let closeButton = document.querySelector('.links__close')
+closeButton.onclick = function() {
+    modalContainer.style.display = 'none'
+}
